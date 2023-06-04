c8bf6a255e5ccb8b22c6a42d389c41c1c4ad4bcc
mani <111manikanta.v@gmail.com>
2023-06-04 12:08:50 +0530

    added new dummy routes

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ app.use(express.json());
 app.get("/", (re, res) => {
     res.send("server is created");
 })
-
+//login
 app.post("/login", (req, res) => {
     console.log("login request came");
     res.send("credentials received!");
     console.log(req.body);
 
 })
+
+//signup
+app.post("/signup", (req, res) => {
+    console.log("signup request came");
+    console.log(req.body);
+    res.send("signup request accepted");
+})
+
+//create problem
+app.post("/createproblem", (req, res) => {
+    console.log("createProblem request came");
+    console.log(req.body);
+    res.send("creating new problem");
+})
+
+app.post("/submitproblem", (req, res) => {
+    console.log(req.body);
+    res.send("evaluating submission");
+})
+
 app.listen(port, () => {
     console.log(`server is listening on port number ${port}`);
 })
