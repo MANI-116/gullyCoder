95314bf0baa8e11c16f9557cc76077c0d2223a7b
mani <111manikanta.v@gmail.com>
2023-06-04 11:45:14 +0530

    login route is added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 app.get("/", (re, res) => {
     res.send("server is created");
 })
 
+app.post("/login", (req, res) => {
+    console.log("login request came");
+    res.send("credentials received!");
+    console.log(req.body);
+
+})
 app.listen(port, () => {
     console.log(`server is listening on port number ${port}`);
 })
